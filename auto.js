e91964f9c498ddb44fed913aa8dd2354a4bdb656
en129it <iso.rdis.team@gmail.com>
2017-02-26 23:26:23 +0100

    Update auto.js

--- auto.js
+++ auto.js
@@ -3,11 +3,58 @@ angular.module("MyRegistry", ['ngRoute', 'ngMaterial'])
 .controller('MyController', MyController)
 .controller('MyAccountController', MyAccountController)
 .controller('AutocompleteController', AutocompleteController)
+.controller('TextHighlighterController', TextHighlighterController)
+.controller('AutocompleteChipsController', AutocompleteChipsController)
 .directive('chipsAutocomplete', ChipsAutocomplete)
+.directive('textHighlighter', TextHighlighter)
+.directive('autocompleteChips', AutocompleteChips)
 ;
 
-// Usage : <chips-autocomplete placeholder="Here..." minchars="3" searchtext="searchText" suggestions="item in myCont.getMatches(searchText)"></chips-autocomplete>
+/*
 
+<chips-autocomplete placeholder="Here..." 
+					minchars="3" 
+					searchtext="searchText" 
+					suggestions="myCont.getMatches(searchText)" 
+					selected-suggestion="myCont.selectedSuggestion">
+	<suggestion-item-template>
+		<div>
+			<div>Suggestion</div>
+			<div><text-highlighter text="searchText">{{suggestionItem}}</text-highlighter></div>
+		</div>
+	</suggestion-item-template>
+</chips-autocomplete>
+
+
+.selected {
+	background-color: blue;
+}
+
+.highlight{
+	font-weight: bold;
+}
+
+.autocomplete-suggestions-container {
+	max-height: 140px;
+	overflow-x: hidden;
+	overflow-y: scroll;
+	position: relative;
+	top: 0px;
+	left: 0px;
+	right: 0px;
+	bottom: auto;
+	z-index:999;
+	background-color: #cacaca;
+}
+
+.autocomplete-suggestions-scroller {
+	position: relative;
+	top: 0px;
+	left: 0px;
+	right: 0px;
+}
+
+*/
 
 /* @nginject */
 function AutocompleteController($scope, $element, $mdUtil, $mdConstant, $mdTheming, $window, $animate, $rootElement, $attrs, $q, $timeout, $rootScope) {
@@ -20,9 +67,11 @@ function AutocompleteController($scope, $element, $mdUtil, $mdConstant, $mdThemi
 	this.hideSuggestions = true;
 	this.selectedSuggestionIndex = -1;
 	this.inputElem = null;
+	this.isUserInSuggestionArea = false;
+	this.suggestionContainerElem = null;
+	this.suggestionScrollerElem = null;
 	
 	this.scope.searchText = "";
-	this.suggestionsExprParts = this.scope.suggestionsExpr.split(/ in /i);
 
 	
 	var self = this;
@@ -34,9 +83,10 @@ function AutocompleteController($scope, $element, $mdUtil, $mdConstant, $mdThemi
 			event.preventDefault();
 			if (!this.hideSuggestions) {
 				this.selectedSuggestionIndex += 1;
-				if (this.selectedSuggestionIndex>this.suggestions.length) {
+				if (this.selectedSuggestionIndex>=this.suggestions.length) {
 					this.selectedSuggestionIndex = 0;
 				}
+				this.scrollToSelectedSuggestion();
 			}
 			break;
 		  case 38: // key up arrow
@@ -47,6 +97,7 @@ function AutocompleteController($scope, $element, $mdUtil, $mdConstant, $mdThemi
 				if (this.selectedSuggestionIndex<0) {
 					this.selectedSuggestionIndex = this.suggestions.length-1;
 				}
+				this.scrollToSelectedSuggestion();
 			}
 			break;
 		  case 13: // key enter
@@ -59,6 +110,8 @@ function AutocompleteController($scope, $element, $mdUtil, $mdConstant, $mdThemi
 		  case 27: // key esc
 			event.stopPropagation();
 			event.preventDefault();
+			this.isUserInSuggestionArea = false;
+			this.blur();
 			this.clearAll();
 			break;
 		}
@@ -66,6 +119,9 @@ function AutocompleteController($scope, $element, $mdUtil, $mdConstant, $mdThemi
 	
 	this.blur = function() {
 		console.log("BLUR");
+		if (!this.isUserInSuggestionArea) {
+			this.closeSugggestions();
+		}
 	};
 	
 	this.focus = function() {
@@ -81,9 +137,15 @@ console.log(">>>>>> select suggestion " + self.suggestions[index]);
 	};
 
 	this.closeSugggestions = function() {
+		this.isUserInSuggestionArea = false;
 		this.inputElem.focus();
 		this.hideSuggestions = true;
+
 	};
+	
+	this.suggestionsEnter = function() {
+		this.isUserInSuggestionArea = true;
+	}
 	
 	this.clearAll = function() {
 		this.scope.searchText = "";
@@ -93,10 +155,12 @@ console.log(">>>>>> select suggestion " + self.suggestions[index]);
 	
 	this.searchSuggestions = function(previousSearchText) {
 		var searchText = this.scope.searchText || '';
+		this.hideSuggestions = true;
+		this.selectedSuggestionIndex = -1;
+		
 		if (searchText!=previousSearchText) {
 			if (searchText.length>=this.scope.minChars) {
-				console.log("SEARCH_SUGGESTIONS " + this.scope.searchText);
-				var suggestionsPromise = this.scope.$parent.$eval(this.suggestionsExprParts[1]);
+				var suggestionsPromise = this.scope.$parent.$eval(this.scope.suggestionsExpr);
 				
 				suggestionPromiseProcessing = true;
 				
@@ -106,6 +170,12 @@ console.log(">>>>>> select suggestion " + self.suggestions[index]);
 							if (searchText==self.scope.searchText) {
 								self.suggestions = results;
 								self.hideSuggestions = (results==null) || (results.length==0);
+								if (!self.hideSuggestions) {
+									self.asyncExecutor(function() {
+										self.suggestionContainerElem.scrollTop=0;
+									});
+
+								}
 							}
 						});
 					}
@@ -156,10 +226,31 @@ console.log(">>>>>> select suggestion " + self.suggestions[index]);
 			$rootScope.$digest();
 		}
 	};
+	
+	this.scrollToSelectedSuggestion = function() {
 
+		var liElems = $element.find('li', this.suggestionScrollerElem);
+		if (liElems.length>0) {
+			var liElemHeight = liElems[0].offsetHeight;
+
+			var scrollerElemTop = this.suggestionContainerElem.scrollTop;
+			var selectedLiElemTop = this.selectedSuggestionIndex * liElemHeight;
+			if (selectedLiElemTop<scrollerElemTop) {
+				this.suggestionContainerElem.scrollTop=selectedLiElemTop;
+			} else {
+				var scrollerElemHeight = this.suggestionContainerElem.clientHeight;
+				if ((selectedLiElemTop+liElemHeight)>(scrollerElemTop+scrollerElemHeight)) {
+					this.suggestionContainerElem.scrollTop=selectedLiElemTop+liElemHeight-scrollerElemHeight;
+				}
+			}
+		}
+	};
 	
     this.scope.$watch('searchText', this.triggerSearchSuggestions);
+	
 	this.inputElem = $element.find('input')[0];
+	this.suggestionContainerElem = $element[0].getElementsByClassName('autocomplete-suggestions-container')[0];
+	this.suggestionScrollerElem = $element[0].getElementsByClassName('autocomplete-suggestions-scroller')[0];
 }
 
 
@@ -177,6 +268,8 @@ function ChipsAutocomplete() {
 		},
 		template: function (element, attr) {
 			var tabIndex = attr.tabIndex;
+			var suggestionTemplate = element.find('suggestion-item-template').detach().html();
+			
 			return '\
 				<div class="autocomplete-container">\
 					<input 	type="search" ' + (tabIndex != null ? 'tabindex="' + tabIndex + '"' : '') + '\
@@ -191,19 +284,88 @@ function ChipsAutocomplete() {
 							placeholder="{{placeholder}}"\
 					/>\
 					<div class="autocomplete-suggestions-container"\
-					  ng-mouseenter="$ctrl.listEnter()"\
+					  ng-mouseenter="$ctrl.suggestionsEnter()"\
 					  ng-mouseleave="$ctrl.closeSugggestions()"\
 					  ng-mouseup="$ctrl.closeSugggestions()"\
-					  ng-hide="$ctrl.showSuggestions">\
-						<ul class="autocomplete-suggestions">\
-							<li ng-repeat="item in $ctrl.suggestions"\
-								ng-class="{selected: $index==$ctrl.selectedSuggestionIndex}"\
-								ng-click="$ctrl.selectSuggestion($index)">\
-							{{item}}\
-							</li>\
-						</ul>\
+					  ng-hide="$ctrl.hideSuggestions">\
+						<div class="autocomplete-suggestions-scroller">\
+						<div class="sizer">\
+							<ul class="autocomplete-suggestions">\
+								<li ng-repeat="suggestionItem in $ctrl.suggestions"\
+									ng-class="{selected: $index==$ctrl.selectedSuggestionIndex}"\
+									ng-click="$ctrl.selectSuggestion($index)">\
+					' + suggestionTemplate +			
+						'		</li>\
+							</ul>\
+						</div>\
+						</div>\
 					</div>\
                 </div>';
 		}
 	};
 }
+
+function TextHighlighterController($scope, $element, $attrs) {
+	
+	this.apply = function(textExpr, innerContentInterpolate) {
+		var isFirst = true;
+		$scope.$watch(
+			function($scope) {
+			  return {
+				text: textExpr($scope),
+				innerContent: innerContentInterpolate($scope)
+			  };
+			}, 
+			function (newScopeState, oldScopeState) {
+				if (isFirst || (newScopeState.text!==oldScopeState.text) || (newScopeState.innerContent!=oldScopeState.innerContent)) {
+					isFirst = false;
+					var adjustedInnterContent = newScopeState.innerContent.replace(new RegExp(newScopeState.text, 'i'), '<span class="highlight">$&</span>');
+					$element.html(adjustedInnterContent);
+				}
+			}, 
+			true);
+	};
+}
+
+function TextHighlighter($interpolate, $parse) {
+	return {
+		terminal: true,
+		controller: 'TextHighlighterController',
+		compile: function textHighlighterCompile(tElement, tAttr) {
+			var textExpr = $parse(tAttr.text);
+			var innerContentInterpolate = $interpolate(tElement.html());
+			return {
+				post: function (scope, element, attr, ctrl) {
+					ctrl.apply(textExpr, innerContentInterpolate);
+				}
+			};
+		}
+    };
+}
+
+function AutocompleteChipsController($scope, $element, $attrs) {
+//	this.chips = $scope.datasource;
+	this.chips = ['a', 'b'];
+}
+
+function AutocompleteChips($interpolate, $parse) {
+	return {
+		controller: 'AutocompleteChipsController',
+		controllerAs: '$ctrl',
+		scope: {
+			datasource: '@datasource'
+		},
+		template: function (element, attr) {
+			var chipTemplate = element.find('chip-item-template').detach().html();
+			return '\
+				<div class="autocomplete-chips-container">\
+					<ul>\
+						<li ng-repeat="chip in $ctrl.chips">\
+			' + chipTemplate +			
+						'</li>\
+					</ul>\
+                </div>';
+		}
+    };
+}
+
